37f5c63b52833efc778f0e8d707c391e6e274087
Sumesh <sumeshdey2222@gmail.com>
2024-10-18 17:29:29 +0530

    second commit

---
+++ scm2.0/src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,25 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelExtensionsKt;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+
+        System.out.println("Home Page Handler");
+        model.addAttribute("name", "Substring");
+        model.addAttribute("youtubechannel", "learnwithdurgesh");
+        model.addAttribute("githubrepo", "https://github.com/Sumesh921/HTMLForms");
+
+
+        return "home";
+
+    }
+
+
+}
